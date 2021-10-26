636958d48023e92f5576726b61771ea38198f475
faten.ghamdi <Faten.arg@hotmail.com>
2021-10-26 09:34:08 +0300

    2end

--- app/src/main/java/com/example/notesapp/DBHlpr.kt
+++ app/src/main/java/com/example/notesapp/DBHlpr.kt
@@ -1,14 +1,20 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.icu.util.Currency
 import android.provider.SyncStateContract.Helpers.insert
+import android.util.Log
 
-class DBHlpr(context: Context?) : SQLiteOpenHelper(context, "notes.db",null, 1) {
+class DBHlpr(context: Context?) : SQLiteOpenHelper(context, "notes.db", null, 1) {
 
-    var sqlDB : SQLiteDatabase =writableDatabase // create database variable
+    var sqlDB: SQLiteDatabase = writableDatabase // create database variable
+    val sqlDBRead:SQLiteDatabase=readableDatabase
+
 
     override fun onCreate(db: SQLiteDatabase?) {
         if (db != null) { // cteate table
@@ -20,16 +26,34 @@ class DBHlpr(context: Context?) : SQLiteOpenHelper(context, "notes.db",null, 1) 
     }
 
 
-    fun saveData(s1:String):Long{ // to return : Long
+    fun saveData(s1: String): Long { // to return : Long
         // add data to ContentValues
         val cv = ContentValues()
         cv.put("notes", s1)
         // add to database 3 para (name of table , null , contentValuse)
-        var s = sqlDB.insert("notes",null , cv)
-            return s
+        var s = sqlDB.insert("notes", null, cv)
+        return s
         // to check data saved or not
         // var statuse= sqlDB.insert("student",null , cv)
         // return statuse
         // return -1 not saved
     }
+
+    fun retrive(): ArrayList<String> {
+        val list = ArrayList<String>()
+        val c: Cursor = sqlDBRead.query("notes", null, null, null, null, null, null)
+       if ( c.moveToFirst()){
+           var note = c.getString(c.getColumnIndex("notes"))
+           list.add(note)
+        while (c.moveToNext()) {
+            note = c.getString(c.getColumnIndex("notes"))
+            list.add(note)        }
+
+        }
+        c.close()
+        return list
+    }
 }
+
+
+
--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -12,23 +13,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rv: RecyclerView
     private lateinit var edNote: EditText
     private lateinit var btnSub: Button
+    private lateinit var notes : ArrayList<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var dbhr = DBHlpr(applicationContext)
+
 
         rv = findViewById(R.id.rv)
         edNote = findViewById(R.id.edNote)
         btnSub = findViewById(R.id.btnSub)
 
+        // get data and put it into rv
+        notes = dbhr.retrive()
+        rv.adapter = MyAdap(this,notes)
+        rv.layoutManager = LinearLayoutManager(this)
+
         btnSub.setOnClickListener {
 
             var s1 = edNote.text.toString()
-            var dbhr = DBHlpr(applicationContext)
-            var s = dbhr.saveData(s1)
-            Toast.makeText(applicationContext,"notes added $s", Toast.LENGTH_LONG).show()
-            edNote.text.clear()
+            if (s1.isNotEmpty()) {
+                var s = dbhr.saveData(s1)
+                notes = dbhr.retrive()
+                rv.adapter = MyAdap(this,notes)
+                rv.layoutManager = LinearLayoutManager(this)
+                Toast.makeText(applicationContext, "notes added $s", Toast.LENGTH_LONG).show()
+                edNote.text.clear()
+            }else {
+                Toast.makeText(applicationContext, "please enter anything", Toast.LENGTH_LONG).show()
 
+            }
 
         }
     }
